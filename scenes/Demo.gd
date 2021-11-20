44d71a132c600c0a36c6ea51b9e91705bd8b7418
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-11-20 14:55:58 -0600

    Working ladders

--- scenes/Demo.gd
+++ scenes/Demo.gd
@@ -3,6 +3,8 @@ extends Node2D
 var debug_overlay
 
 func _ready() -> void:
+	var explorer = $Explorer
 	debug_overlay = $DebugOverlay
-	debug_overlay.add_stat("Direction X", $Explorer, 'direction', false)
+	debug_overlay.add_stat("Direction X", explorer, 'direction', false)
+	debug_overlay.add_stat('In Ladder', explorer, '_in_ladder', false)
 
