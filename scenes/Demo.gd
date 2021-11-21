2368dcd8923e3e2e7c6427eeab2a831244a7a055
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-11-20 18:07:45 -0600

    Add energy ball to level

--- scenes/Demo.gd
+++ scenes/Demo.gd
@@ -7,4 +7,8 @@ func _ready() -> void:
 	debug_overlay = $DebugOverlay
 	debug_overlay.add_stat("Direction X", explorer, 'direction', false)
 	debug_overlay.add_stat('In Ladder', explorer, '_in_ladder', false)
+	debug_overlay.add_stat('Picked', $EnergyBall, 'picked', false)
 
+func _unhandled_key_input(event: InputEventKey) -> void:
+	if event.is_action_pressed('reset'):
+		$EnergyBall.set_rigid_position(Vector2(248, 350))
